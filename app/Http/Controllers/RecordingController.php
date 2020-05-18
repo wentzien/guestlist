dfd2bd35f535daa1c7ec7ec4343e240a35e23c21
Dennis Wentzien <wentzien@users.noreply.github.com>
2020-05-18 18:44:01 +0200

    individual url for gastro

--- app/Http/Controllers/RecordingController.php
+++ app/Http/Controllers/RecordingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RecordingController extends Controller
 {
@@ -18,7 +19,16 @@ class RecordingController extends Controller
                 'text_to_speech' => $welcome_data->welcome_heading." ".$welcome_data->welcome_text
             ]);
         } elseif ($gastro_name != null) {
-
+            $welcome_data = DB::table('users')->where('gastro_name', $gastro_name)->first();
+            if($welcome_data){
+                return view("recording", [
+                    'welcome_heading' => $welcome_data->welcome_heading,
+                    'welcome_text' => $welcome_data->welcome_text,
+                    'text_to_speech' => $welcome_data->welcome_heading." ".$welcome_data->welcome_text
+                ]);
+            }else{
+                return $this->show(null);
+            }
         } else {
             //ansonsten Standartwerte
             return view("recording", [
