92864cdcf71f7affbcfcc03b9766d462378bd8cd
Dennis Wentzien <wentzien@users.noreply.github.com>
2020-05-26 09:46:16 +0200

    navbar fix

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::put('/settings/update', 'SettingController@update');
 Route::get('/welcome/{gastro_name}', 'WelcomeController@show')->name('welcome');
 Route::get('/welcome', 'WelcomeController@show')->name('welcome');
 //Slideshow mit Audio-Texten und Aufzeichnung der pers. Daten
-Route::get('/recording/{gastro_name}', 'RecordingController@show');
+Route::get('/recording/{gastro_name}', 'RecordingController@show')->name('recording');
 Route::get('/recording', 'RecordingController@show')->name('recording');
 
 Auth::routes();
